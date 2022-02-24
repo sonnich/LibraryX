28aa703a5f803355043fc716faa6851896ae8f24
Sonnich <sonnichj@gmail.com>
2022-02-24 16:36:31 +0100

    BookActivity reads parcelable book

--- app/src/main/java/com/example/libraryx/BookActivity.java
+++ app/src/main/java/com/example/libraryx/BookActivity.java
@@ -2,6 +2,7 @@ package com.example.libraryx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,29 +15,22 @@ public class BookActivity extends AppCompatActivity {
     private Button btnAddCurrent, btnAddWant, btnAddAlreadyRead, btnAddFavourite;
     private ImageView imgCover;
 
+    private static final String BOOK = "book";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
 
+        Intent intent = getIntent();
+        Book b1 = intent.getParcelableExtra(BOOK);
+
         initViews();
 
 
-         String longTest = "A young woman named Aomame follows a taxi driver’s enigmatic suggestion" +
-                " and begins to notice puzzling discrepancies in the world around her." +
-                " She has entered, she realizes, a parallel existence, which she calls 1Q84 —“Q is for ‘question mark.’" +
-                " A world that bears a question.” Meanwhile, an aspiring writer named Tengo takes on" +
-                " a suspect ghostwriting project. He becomes so wrapped up with the work and its unusual author that, " +
-                "soon, his previously placid life begins to come unraveled.\n\nAs Aomame’s and Tengo’s narratives converge" +
-                 " over the course of this single year, we learn of the profound and tangled connections that bind them ever " +
-                 "closer: a beautiful, dyslexic teenage girl with a unique vision; a mysterious religious cult that instigated" +
-                 " a shoot-out with the metropolitan police; a reclusive, wealthy dowager who runs a shelter for abused women;" +
-                 " a hideously ugly private investigator; a mild-mannered yet ruthlessly efficient bodyguard; and a peculiarly" +
-                 " insistent television-fee collector.";
 
 
-        //TODO fix dummy data
-        Book b1 = new Book(1, "1q84", "Murikami", 500, "book about 1 q and 84", longTest, "https://imgcdn.saxo.com/_9780099578079"  );
+
 
         setData(b1);
 
--- app/src/main/java/com/example/libraryx/Utils.java
+++ app/src/main/java/com/example/libraryx/Utils.java
@@ -45,8 +45,19 @@ public class Utils {
 
 
     private void initData(){
+        String longTest = "A young woman named Aomame follows a taxi driver’s enigmatic suggestion" +
+                " and begins to notice puzzling discrepancies in the world around her." +
+                " She has entered, she realizes, a parallel existence, which she calls 1Q84 —“Q is for ‘question mark.’" +
+                " A world that bears a question.” Meanwhile, an aspiring writer named Tengo takes on" +
+                " a suspect ghostwriting project. He becomes so wrapped up with the work and its unusual author that, " +
+                "soon, his previously placid life begins to come unraveled.\n\nAs Aomame’s and Tengo’s narratives converge" +
+                " over the course of this single year, we learn of the profound and tangled connections that bind them ever " +
+                "closer: a beautiful, dyslexic teenage girl with a unique vision; a mysterious religious cult that instigated" +
+                " a shoot-out with the metropolitan police; a reclusive, wealthy dowager who runs a shelter for abused women;" +
+                " a hideously ugly private investigator; a mild-mannered yet ruthlessly efficient bodyguard; and a peculiarly" +
+                " insistent television-fee collector.";
         //TODO populate starting data;
-        Book b1 = new Book(1, "1q84", "Murikami", 500, "book about 1 q and 84", "very long book", "https://imgcdn.saxo.com/_9780099578079"  );
+        Book b1 = new Book(1, "1q84", "Murikami", 500, "book about 1 q and 84", longTest, "https://imgcdn.saxo.com/_9780099578079"  );
         Book b2 = new Book(2, "The Hobbit", "Tolkien", 400, "Book about a hobbit", "book about a hobbit and some dwarves, a wizard, and a dragon."
                 ,"https://cdn3.tales.dk/thumbnail/200x0/00019/42606/cover.1497233143.jpg");
         allBooksList.add(b1);
