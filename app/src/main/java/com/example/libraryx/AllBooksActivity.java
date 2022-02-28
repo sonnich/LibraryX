533169612554db5d7251a838ac9863f3e71b86c4
Sonnich <sonnichj@gmail.com>
2022-02-28 14:58:39 +0100

    Added animation files. Does not work Labels for top bar. One backbutton in topbar.

--- app/src/main/java/com/example/libraryx/AllBooksActivity.java
+++ app/src/main/java/com/example/libraryx/AllBooksActivity.java
@@ -1,11 +1,13 @@
 package com.example.libraryx;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -21,6 +23,11 @@ public class AllBooksActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //using custom animation causes a bug with black screen.
+        //overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+
         adapter = new BookRecViewAdapter(this, TAG);
         initView();
         allBooksRec.setAdapter(adapter);
@@ -33,5 +40,15 @@ public class AllBooksActivity extends AppCompatActivity {
 
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
